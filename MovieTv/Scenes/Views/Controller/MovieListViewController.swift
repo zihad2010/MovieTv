9ab2d03eb52bfab9672c180a78fdd96e54d6388b
zihad2010 <asrafulalam2010502cse@gmail.com>
2021-06-10 03:35:47 +0600

    added UICollectionViewDelegateFlowLayout

--- MovieTv/Scenes/Views/Controller/MovieListViewController.swift
+++ MovieTv/Scenes/Views/Controller/MovieListViewController.swift
@@ -11,7 +11,7 @@ import RxCocoa
 import Alamofire
 
 class MovieListViewController: UIViewController {
-   
+    
     private let disposable = DisposeBag()
     private var viewModel = MovieListViewModel()
     var coordinator: MovieListCoordinator?
@@ -22,15 +22,28 @@ class MovieListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBindings()
-        
+        self.movieListCollectionView.rx.setDelegate(self).disposed(by: disposable)
     }
-
+    
     private func setupBindings() {
         
         viewModel.MovieList.observeOn(MainScheduler.instance)
             .bind(to: movieListCollectionView.rx.items(cellIdentifier: "MovieListCollectionViewCell", cellType: MovieListCollectionViewCell.self)) {  (row,eachMovie,cell) in
                 cell.eachMovie = eachMovie
             }.disposed(by: disposable)
+        
+        movieListCollectionView.rx.itemSelected.subscribe { indexPath in
+            
+        }.disposed(by: disposable)
     }
     
+}
+
+extension MovieListViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width
+        let cellWidth = (width - 30) / 3 // compute your cell width
+        return CGSize(width: cellWidth, height: cellWidth / 0.6)
+    }
 }
